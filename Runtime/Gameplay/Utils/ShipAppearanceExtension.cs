a598538d25abfb2844ca4c44d9bd610f3ef0d3b6
Hostur <f.zielinski.gliwice@gmail.com>
2025-01-10 15:03:34 +0100

    backup

---
+++ Runtime/Gameplay/Utils/ShipAppearanceExtension.cs
@@ -0,0 +1,25 @@
+﻿using BIG;
+using BIG.Types;
+using SpaceSmuggler.Gameplay.Types;
+using SpaceSmuggler.Gameplay.Types.Enums;
+
+namespace SpaceSmuggler.Gameplay.Utils
+{
+    public static class ShipAppearanceExtension
+    {
+        public static ShipAppearance Randomize(this ShipAppearance shipAppearance)
+        {
+            shipAppearance.Name = "Test";
+            shipAppearance.ColorA = Color.Default.Randomize();
+            shipAppearance.ColorB = Color.Default.Randomize();
+            shipAppearance.ColorC = Color.Default.Randomize();
+            shipAppearance.Emission = Color.Default.Randomize();
+            shipAppearance.ThrustersColor = Color.Default.Randomize();
+
+            shipAppearance.DecalA = ShipDecal.None;
+            shipAppearance.DecalB = ShipDecal.None;
+            shipAppearance.SpecialTexture = SpecialTexture.None;
+            return shipAppearance;
+        }
+    }
+}
